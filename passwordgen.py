6f24372bda618a5712de95d439a809f9196d8a3e
mas <mail.adhimas@gmail.com>
2022-11-21 22:13:46 +0700

    First Commit

---
+++ passwordgen.py
@@ -0,0 +1,34 @@
+import random
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "!@#$%^&*()-_=+[{]};:<,>.?/\|`~ "
+
+upper, lower, nums, syms = True, True, True, True
+
+all = ""
+
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
+
+length = 50
+amount = 10
+
+#You can use seed for this password generator
+#But I dont know why you want to use seed to get the same password in a random password generator
+#I don't recommend this but you can use seed for this password generator
+
+#seed = "anything"
+
+#random.seed(seed)
+
+for x in range(amount):
+    password = "".join(random.sample(all, length))
+    print(password)
